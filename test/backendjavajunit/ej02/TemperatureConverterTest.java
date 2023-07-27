3cbe252852a87ea07485093fccdec716f6df9770
Juan Duarte <juan.duarte.a@gmail.com>
2023-07-27 14:58:30 -0500

    Modificadas pruebas ej2 para acceder a métodos estáticos

--- test/backendjavajunit/ej02/TemperatureConverterTest.java
+++ test/backendjavajunit/ej02/TemperatureConverterTest.java
@@ -18,62 +18,62 @@ public class TemperatureConverterTest {
 
     @Test
     public void testCalculateCelsiusToCelsius() {
-        assertEquals(100.0, tc.calculateCelsius(temperature, Units.Celsius), 0);
+        assertEquals(100.0, TemperatureConverter.calculateCelsius(temperature, Units.Celsius), 0);
     }
 
     @Test
     public void testCalculateCelsiusToFahrenheit() {
-        assertEquals(212.0, tc.calculateFahrenheit(temperature, Units.Celsius), 0);
+        assertEquals(212.0, TemperatureConverter.calculateFahrenheit(temperature, Units.Celsius), 0);
     }
 
     @Test
     public void testCalculateCelsiusToKelvin() {
-        assertEquals(373.15, tc.calculateKelvin(temperature, Units.Celsius), 0);
+        assertEquals(373.15, TemperatureConverter.calculateKelvin(temperature, Units.Celsius), 0);
     }
 
     @Test
     public void testCalculateFahrenheitToFahrenheit() {
-        assertEquals(100.0, tc.calculateFahrenheit(temperature, Units.Fahrenheit), 0);
+        assertEquals(100.0, TemperatureConverter.calculateFahrenheit(temperature, Units.Fahrenheit), 0);
     }
 
     @Test
     public void testCalculateFahrenheitToCelsius() {
-        assertEquals(37.778, tc.calculateCelsius(temperature, Units.Fahrenheit), 0.001);
+        assertEquals(37.778, TemperatureConverter.calculateCelsius(temperature, Units.Fahrenheit), 0.001);
     }
 
     @Test
     public void testCalculateFahrenheitToKelvin() {
-        assertEquals(310.928, tc.calculateKelvin(temperature, Units.Fahrenheit), 0.001);
+        assertEquals(310.928, TemperatureConverter.calculateKelvin(temperature, Units.Fahrenheit), 0.001);
     }
 
     @Test
     public void testCalculateKelvinToKelvin() {
-        assertEquals(100.0, tc.calculateKelvin(temperature, Units.Kelvin), 0);
+        assertEquals(100.0, TemperatureConverter.calculateKelvin(temperature, Units.Kelvin), 0);
     }
     
     @Test
     public void testCalculateKelvinToCelsius() {
-        assertEquals(-173.15, tc.calculateCelsius(temperature, Units.Kelvin), 0.01);
+        assertEquals(-173.15, TemperatureConverter.calculateCelsius(temperature, Units.Kelvin), 0.01);
     }
     
     @Test
     public void testCalculateKelvinToFahrenheit() {
-        assertEquals(-279.67, tc.calculateFahrenheit(temperature, Units.Kelvin), 0.01);
+        assertEquals(-279.67, TemperatureConverter.calculateFahrenheit(temperature, Units.Kelvin), 0.01);
     }
     
     @Test
     public void testCalculateCelsiusToKelvinInvalidTemperature() {
-        assertEquals(TemperatureConverter.MIN_KELVIN_TEMP, tc.calculateKelvin(-300, Units.Celsius), 0);
+        assertEquals(TemperatureConverter.MIN_KELVIN_TEMP, TemperatureConverter.calculateKelvin(-300, Units.Celsius), 0);
     }
     
     @Test
     public void testCalculateKelvinToFahrenheitInvalidTemperature() {
-        assertEquals(TemperatureConverter.MIN_FAHRENHEIT_TEMP, tc.calculateFahrenheit(-3, Units.Kelvin), 0);
+        assertEquals(TemperatureConverter.MIN_FAHRENHEIT_TEMP, TemperatureConverter.calculateFahrenheit(-3, Units.Kelvin), 0);
     }
     
     @Test
     public void testCalculateFahrenheitToCelsiusInvalidTemperature() {
-        assertEquals(TemperatureConverter.MIN_CELSIUS_TEMP, tc.calculateCelsius(-460, Units.Fahrenheit), 0);
+        assertEquals(TemperatureConverter.MIN_CELSIUS_TEMP, TemperatureConverter.calculateCelsius(-460, Units.Fahrenheit), 0);
     }
     
 }
